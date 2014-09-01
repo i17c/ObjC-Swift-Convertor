d43e7092c312be04ca96cf2598a87d98fa22282e
Shuyang Sun <sunbuffett@gmail.com>
2014-09-01 11:35:20 -0500

    Created project.

---
+++ ObjC-Swift Convertor/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ObjC-Swift Convertor
+//
+//  Created by Shuyang Sun on 9/1/14.
+//  Copyright (c) 2014 Shuyang Sun. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
